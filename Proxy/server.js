42a25f7be2d039f70a2f8e99f25e888c5552efe6
Jane Lee <sohyunlee53@gmail.com>
2019-01-05 14:25:26 -0800

    rendering Jane component but not fully

--- Proxy/server.js
+++ Proxy/server.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const request = require('request');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', express.static('./public'));
+app.use(/\/\d+\//, express.static('./public'));
+
+app.use('/api/jane/player/:id', (req, res) => {
+  var url = `http://localhost:3002/api/jane/player/${req.params.id}`;
+  req.pipe(request(url)).pipe(res);
+});
+
+// app.use('/api/songs-info', (req, res) => {
+//   var url =
+// })
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
